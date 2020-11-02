fd5a9a2e93ab7d00b521fe7636ed472ab851dc46
alexsoulisweb <alexsoulisdev@outlook.com>
2020-11-02 16:28:24 +0100

    Guess a number

---
+++ guessanumber/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int randomNumber;
+        Random  rand = new Random();
+        randomNumber = rand.nextInt(10)+1;
+
+        while(true){
+            System.out.print("User Input:");
+
+            int userInput = sc.nextInt();
+
+
+            if(userInput==randomNumber){
+                System.out.println("Congratulation");
+
+            }else if(userInput>randomNumber){
+                System.out.println("Guess Lower");
+            }else{
+                System.out.println("Guess Higher");
+            }
+        }
+    }
+}
